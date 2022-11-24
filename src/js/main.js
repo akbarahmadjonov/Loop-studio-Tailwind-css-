5cdb74070189026e880cd466d598795108487fc7
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2022-11-24 18:05:56 +0500

    Initital commit

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+let btn = document.querySelector("#clickup");
+btn.addEventListener("click", function () {
+  alert("Hey, There is nothing bro??");
+  event.target.style.backgroundColor = "#f1f1f1";
+});
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      // backgroundImage: {
+      //   "hero-bg": "url('/images/hero-bg.jpg)",
+      // },
+      backgroundImage: {
+        "hero-pattern": "url('/images/hero-bg.jpg')",
+        "leader-pattern": "url('/images/leader-bg.jpg')",
+      },
+    },
+    plugins: [],
+  },
+};
